44921607c53e0b71087d5ea0969746ffc3ac99d4
Zhongling Ji <jizhongling@gmail.com>
2022-01-31 22:59:40 -0500

    Read dataset in libtorch

--- detectors/sPHENIX/LoadTorchScript.cpp
+++ detectors/sPHENIX/LoadTorchScript.cpp
@@ -1,6 +1,13 @@
-// g++ -I$OFFLINE_MAIN/include -L$OFFLINE_MAIN/lib -lc10 -ltorch_cpu -ltorch -o LoadTorchScript LoadTorchScript.cpp
+// g++ -Wall -I$OFFLINE_MAIN/include -L$OFFLINE_MAIN/lib `root-config --cflags --glibs` -lc10 -ltorch_cpu -ltorch -o LoadTorchScript LoadTorchScript.cpp
 #include <iostream>
 #include <memory>
+#include <array>
+#include <vector>
+#include <algorithm>
+
+#include <TMath.h>
+#include <TFile.h>
+#include <TNtuple.h>
 
 // One-stop header
 #include <torch/script.h>
@@ -9,9 +16,9 @@ using namespace std;
 
 int main(int argc, const char *argv[])
 {
-  if(argc != 2)
+  if(argc != 4)
   {
-    cerr << "Usage: " << argv[0] << " <path-to-exported-script-module>" << endl;
+    cerr << "Usage: " << argv[0] << " <script-module> <root-file> <ientry>" << endl;
     return 1;
   }
 
@@ -21,20 +28,63 @@ int main(int argc, const char *argv[])
     // Deserialize the ScriptModule from a file using torch::jit::load()
     module = torch::jit::load(argv[1]);
   }
-  catch(const c10::Error& e)
+  catch(const c10::Error &e)
   {
     cerr << "Error: cannot load model " << argv[1] << endl;
     return 1;
   }
   cout << "Load model " << argv[1] << endl;
 
+  auto f = new TFile(argv[2]);
+  if(f->IsZombie())
+  {
+    cerr << "Error: cannot open file " << argv[2] << endl;
+    return 1;
+  }
+  cout << "Open file " << argv[2] << endl;
+
+  const size_t nd = 10;
+  array<Short_t, (2*nd+1)*(2*nd+1)> v_adc;
+  array<Float_t, (2*nd+1)*(2*nd+1)> v_gedep;
+  Short_t li;
+  Float_t zr;
+  vector<Float_t> *v_reco_phi = 0;
+  vector<Float_t> *v_reco_z = 0;
+  Short_t nreco;
+  vector<Float_t> *v_truth_phi = 0;
+  vector<Float_t> *v_truth_z = 0;
+  Short_t ntruth;
+
+  TTree *T = static_cast<TTree*>(f->Get("T"));
+  T->SetBranchAddress("adc", &v_adc);
+  T->SetBranchAddress("gedep", &v_gedep);
+  T->SetBranchAddress("layer", &li);
+  T->SetBranchAddress("ztan", &zr);
+  T->SetBranchAddress("reco_phi", &v_reco_phi);
+  T->SetBranchAddress("reco_z", &v_reco_z);
+  T->SetBranchAddress("nreco", &nreco);
+  T->SetBranchAddress("truth_phi", &v_truth_phi);
+  T->SetBranchAddress("truth_z", &v_truth_z);
+  T->SetBranchAddress("ntruth", &ntruth);
+
+  T->GetEntry(stoi(string(argv[3])));
+
   // Create a vector of inputs
   vector<torch::jit::IValue> inputs;
-  inputs.push_back(torch::ones({1, 25}));
+  inputs.push_back(torch::stack({
+        torch::from_blob(vector<Float_t>(v_adc.begin(), v_adc.end()).data(), {1, 21, 21}, torch::kFloat32).sub(75).clamp_min(0),
+        torch::full({1, 21, 21}, li, torch::kFloat32),
+        torch::full({1, 21, 21}, zr, torch::kFloat32)
+        }, 1));
 
   // Execute the model and turn its output into a tensor
   at::Tensor output = module.forward(inputs).toTensor();
-  cout << output.slice(/*dim=*/1, /*start=*/0, /*end=*/2) << endl;
+  cout << output.slice(/*dim=*/1, /*start=*/5, /*end=*/16).slice(2, 5, 16) << endl;
+  cout << "Output value at Row 4(9) Column 6(11) is " << output.data_ptr<float>()[8*21+10] << endl;
+
+  at::Tensor target = torch::from_blob(vector<Float_t>(v_gedep.begin(), v_gedep.end()).data(), {1, 21, 21}, torch::kFloat32).mul(1e6).clamp_max(10);
+  cout << target.slice(/*dim=*/1, /*start=*/5, /*end=*/16).slice(2, 5, 16) << endl;
+  cout << "Target value at Row 4(9) Column 6(11) is " << target.data_ptr<float>()[8*21+10] << endl;
 
   return 0;
 }
